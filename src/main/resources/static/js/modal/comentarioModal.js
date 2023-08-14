b66c18f48577f01788482f70ac7f7d7e491e49ad
tomasdeleon <deleontomas99@gmail.com>
2023-08-14 00:43:01 -0300

    version SUCODER (14/08/2023)

---
+++ src/main/resources/static/js/modal/comentarioModal.js
@@ -0,0 +1,43 @@
+// Function to handle the "Guardar Comentario" button click
+  function saveComment() {
+    const commentMessage = document.getElementById('mensajeComentado').value;
+
+    // Check if the comment is not empty
+    if (commentMessage.trim() === "") {
+      // Show a warning message using SweetAlert
+      swal({
+        title: "Error",
+        text: "Por favor ingrese un comentario.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+      return;
+    }
+
+    addCommentToProcedure(commentMessage); // Pass the comment message as an argument to the function
+  }
+
+  // Function to update the procedure with the entered message
+  function addCommentToProcedure(commentMessage) {
+    const procedureName = document.getElementById('procedureName').value;
+    const variablesGuardados = document.getElementById('variablesCargados').value;
+    const commentStructure = `//${commentMessage}`;
+
+    // Construct the procedure with the entered message and comment
+    const formattedProcedure = `public void ${procedureName}() {\n${variablesGuardados}\n${ifStructure}\n${whileStructure}\n\n${messageStructure}\n\n${commentStructure}\n\n}`;
+
+    // Update the maintextarea with the complete procedure
+    const maintextarea = document.getElementById('maintextarea');
+    maintextarea.value = formattedProcedure;
+
+    const modal = document.getElementById('comentarModal');
+    const bsModal = bootstrap.Modal.getInstance(modal);
+    bsModal.hide();
+
+    // Show a success message using SweetAlert
+    swal({
+      title: 'Mensaje guardado correctamente!',
+      icon: 'success',
+      confirmButtonText: 'OK',
+    });
+  }
---
+++ src/main/resources/static/js/modal/mensajeModal.js
@@ -0,0 +1,44 @@
+// Function to handle the "Guardar Mensaje" button click
+  function saveMessage() {
+    const message = document.getElementById('imprimirMensaje').value;
+
+    // Check if the message is not empty
+    if (message.trim() === "") {
+      // Show a warning message using SweetAlert
+      swal({
+        title: "Error",
+        text: "Por favor ingrese un mensaje.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+      return;
+    }
+
+    addMessageToProcedure(message); // Pass the message as an argument to the function
+  }
+
+  // Function to update the procedure with the entered message
+  function addMessageToProcedure(message) {
+    const procedureName = document.getElementById('procedureName').value;
+    const variablesGuardados = document.getElementById('variablesCargados').value;
+
+    messageStructure = `System.out.println("${message}");`;
+
+    // Construct the procedure with the entered message
+    const formattedProcedure = `public void ${procedureName}() {\n${variablesGuardados}\n${ifStructure}\n${whileStructure}\n\n${messageStructure}\n\n${commentStructure}\n\n}`;
+
+    // Update the maintextarea with the complete procedure
+    const maintextarea = document.getElementById('maintextarea');
+    maintextarea.value = formattedProcedure;
+
+    const modal = document.getElementById('imprimirModal');
+    const bsModal = bootstrap.Modal.getInstance(modal);
+    bsModal.hide();
+
+    // Show a success message using SweetAlert
+    swal({
+      title: 'Mensaje guardado correctamente!',
+      icon: 'success',
+      confirmButtonText: 'OK',
+    });
+  }
