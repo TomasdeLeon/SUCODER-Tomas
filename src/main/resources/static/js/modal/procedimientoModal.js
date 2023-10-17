b69037485424cd645f9e318da25f96214eb5eaec
tomasdeleon <deleontomas99@gmail.com>
2023-10-17 00:39:19 -0300

    version SUCODER 16/10/2023

--- src/main/resources/static/js/modal/procedimientoModal.js
+++ src/main/resources/static/js/modal/procedimientoModal.js
@@ -1,98 +1,85 @@
 // Function to create the PROCEDIMIENTO code
-    function createProcedimientoCode(procedureName) {
-        const procedureCallCode = `${procedureName}();`;
-        return procedureCallCode;
-    }
+function createProcedimientoCode(procedureName) {
+  const procedureCallCode = `${procedureName}();`;
+  return procedureCallCode;
+}
 
-    // Function to generate the procedure code and insert it
-    function generateProcedureCode() {
-        // Get the procedimiento name from the modal field
-        const procedimientoName = document.getElementById('procedimientoName').value;
+// Function to populate procedimientoText
+function populateProcedimientoText() {
+  const procedimientoName = document.getElementById('procedimientoName').value;
 
-        // Generate the code for calling the PROCEDIMIENTO component
-        const procedureCallCode = createProcedimientoCode(procedimientoName);
+  // Replace this logic with your own to fetch the procedure for the user.
+  // For this example, we'll use an empty string.
+  const fetchedProcedimientoText = ''; // Fetch the procedure content
 
-        // Get the existing content of the maintextarea (replace 'maintextarea' with the correct ID)
-        const maintextarea = document.getElementById('maintextarea');
+  const procedimientoText = document.getElementById('procedimientoText');
+  procedimientoText.value = fetchedProcedimientoText;
 
-        // Find the position of the last closing curly brace in the existing content
-        const lastClosingBraceIndex = maintextarea.value.lastIndexOf('}');
+  // After populating procedimientoText, check if it's empty and hide/show the button
+  const insertButton = document.getElementById('insertProcedimiento');
+  if (procedimientoText.value.trim() === '') {
+    insertButton.disabled = false; // Hide the button
+  } else {
+    insertButton.disabled = true; // Show the button
+  }
+}
 
-        // Insert the generated procedure call code before the last closing curly brace
-        const updatedContent = insertCodeAtLocation(maintextarea.value, procedureCallCode, clickedLineNumber, lastClosingBraceIndex);
+// Add an event listener to the "searchProcedimiento" button
+document.getElementById('searchProcedimiento').addEventListener('click', function () {
+  populateProcedimientoText();
+});
 
-        // Update the maintextarea with the new content
-        maintextarea.value = updatedContent;
+// Function to generate the procedure code and insert it
+function generateProcedureCode() {
+  const procedimientoName = document.getElementById('procedimientoName').value;
+  const procedureCallCode = createProcedimientoCode(procedimientoName);
+  const maintextarea = document.getElementById('maintextarea');
+  const lastClosingBraceIndex = maintextarea.value.lastIndexOf('}');
+  const updatedContent = insertCodeAtLocation(maintextarea.value, procedureCallCode, clickedLineNumber, lastClosingBraceIndex);
+  maintextarea.value = updatedContent;
 
-        // Get the procedimientoText and split it into lines
-        const procedimientoText = document.getElementById('procedimientoText').value;
-        const procedimientoTextLines = procedimientoText.split('\n');
+  // Get the procedimientoText and split it into lines
+  const procedimientoText = document.getElementById('procedimientoText').value;
+  const procedimientoTextLines = procedimientoText.split('\n');
 
-        // Add a comment message
-        maintextarea.value += `\n// Procedimiento ${procedimientoName}\n`;
+  // Add a comment message
+  maintextarea.value += `\n// Procedimiento ${procedimientoName}\n`;
 
-        // Add procedimientoText lines as comments outside the main method code
-        for (const line of procedimientoTextLines) {
-            maintextarea.value += `\n// ${line}`;
-        }
-        maintextarea.value += '\n'; // Add an extra line for separation
+  // Add procedimientoText lines as comments outside the main method code
+  for (const line of procedimientoTextLines) {
+    maintextarea.value += `\n// ${line}`;
+  }
+  maintextarea.value += '\n'; // Add an extra line for separation
 
-        // Close the modal
-        const modal = document.getElementById('procedimientoModal');
-        const bsModal = bootstrap.Modal.getInstance(modal);
-        bsModal.hide();
+  // Close the modal
+  const modal = document.getElementById('procedimientoModal');
+  const bsModal = bootstrap.Modal.getInstance(modal);
+  bsModal.hide();
 
-        // After inserting the procedure, check if the procedimientoText is empty and disable the button
-        toggleInsertButton();
+  // Clear the procedimientoName field
+  document.getElementById('procedimientoName').value = '';
 
-        // Show SweetAlert confirmation
-        swal({
-            icon: 'success',
-            title: 'Procedimiento Insertado',
-        });
-    }
+  // Hide the "Insertar Procedimiento" button after the procedure is inserted
+  const insertButton = document.getElementById('insertProcedimiento');
+  insertButton.style.display = 'none';
+}
 
-    // Function to enable or disable the "Insertar Procedimiento" button
-    function toggleInsertButton() {
-        const procedimientoText = document.getElementById('procedimientoText').value;
-        const insertButton = document.getElementById('insertProcedimiento');
+// Add an event listener to the "Insertar Procedimiento" button
+document.getElementById('insertProcedimiento').addEventListener('click', function () {
+  generateProcedureCode();
+});
 
-        if (procedimientoText.trim() === '') {
-            insertButton.disabled = true;
-        } else {
-            insertButton.disabled = false;
-        }
-    }
+// Function to clear procedimientoText and procedimientoName
+function clearProcedimientoFields() {
+  document.getElementById('procedimientoName').value = '';
+  document.getElementById('procedimientoText').value = '';
 
-    // Add an event listener to the "Insertar Procedimiento" button
-    document.getElementById('insertProcedimiento').addEventListener('click', function () {
-        generateProcedureCode(); // Call the function to insert the procedure call into the maintextarea
-    });
+  // Hide the "Insertar Procedimiento" button when clearing the fields
+  const insertButton = document.getElementById('insertProcedimiento');
+  insertButton.disabled = true;
+}
 
-    // Add an event listener to the "searchProcedimiento" button (if needed)
-    document.getElementById('searchProcedimiento').addEventListener('click', function () {
-        // Add logic to fetch and populate procedimientoText
-        const procedimientoText = "Procedimiento content here.";
-        document.getElementById('procedimientoText').value = procedimientoText;
-        toggleInsertButton();
-    });
-
-    // Call the toggleInsertButton function initially to set the button's initial state
-    toggleInsertButton();
-
-    // Function to clear procedimientoText and procedimientoName
-        function clearProcedimientoFields() {
-            document.getElementById('procedimientoName').value = '';
-            document.getElementById('procedimientoText').value = '';
-            toggleInsertButton();
-        }
-
-        // Add an event listener to clear the fields when the modal is hidden
-        $('#procedimientoModal').on('hidden.bs.modal', function () {
-            clearProcedimientoFields();
-        });
-
-        // Add an event listener to clear the fields when the modal is dismissed
-        $('#procedimientoModal').on('dismiss.bs.modal', function () {
-            clearProcedimientoFields();
-        });
+// Add an event listener to clear the fields when the modal is hidden
+$('#procedimientoModal').on('hidden.bs.modal', function () {
+  clearProcedimientoFields();
+});
